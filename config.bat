d0926875113b0c8b58f3b606cc41f076eae3a67f
D. Dickey <D. Dickey>
2017-03-23 00:28:12 -0500

    Removed harmless warning.

--- config.bat
+++ config.bat
@@ -1,4 +1,6 @@
 xcopy /Y /Q /H /E /K /I %WXDIR%\lib\vc_dll\*u_*.dll ..\buildwin\wxWidgets
 
+if not exist CMakeCache.txt goto config
 del CMakeCache.txt
+:config
 cmake -Wno-dev -G "Visual Studio 14 2015" -T v140_xp ..
