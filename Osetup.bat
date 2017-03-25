e75109c1af562672c0128d3317a78d83e287f36a
transmitterdan <dldickey@ieee.org>
2017-03-25 17:26:22 -0500

    Various cleanup

--- Osetup.bat
+++ Osetup.bat
@@ -1,11 +1,11 @@
 @rem wxWidgets
 @rem -------------------
-@if exist "E:\wxWidgets-2.8.12" set WXDIR=E:\wxWidgets-2.8.12
-@if exist "E:\wxWidgets-3.0.2" set "WXDIR=E:\wxWidgets-3.0.2"
-@if exist "E:\wxWidgets-3.1.0" set "WXDIR=E:\wxWidgets-3.1.0"
-@if exist "E:\storage\transmitterdan\wxWidgets-3.0.2\" set WXDIR=E:\storage\transmitterdan\wxWidgets-3.0.2
-@if exist "E:\storage\transmitterdan\wxwidgets\" set WXDIR=E:\storage\transmitterdan\wxwidgets
-@set "PATH=%WXDIR%;%WXDIR%\lib\vc_dll;e:\storage\transmitterdan\BatchUTILS;%PATH%"
+@if exist "C:\wxWidgets-2.8.12" set WXDIR=C:\wxWidgets-2.8.12
+@if exist "C:\wxWidgets-3.0.2" set "WXDIR=C:\wxWidgets-3.0.2"
+@if exist "C:\wxWidgets-3.1.0" set "WXDIR=C:\wxWidgets-3.1.0"
+@if exist "C:\storage\transmitterdan\wxWidgets-3.0.2\" set WXDIR=C:\storage\transmitterdan\wxWidgets-3.0.2
+@if exist "C:\storage\transmitterdan\wxwidgets\" set WXDIR=C:\storage\transmitterdan\wxwidgets
+@set "PATH=%WXDIR%;%WXDIR%\lib\vc_dll;C:\storage\transmitterdan\BatchUTILS;%PATH%"
 @rem POedit
 @rem -------------------
 @if exist "%ProgramFiles%\Poedit" set "PATH=%ProgramFiles%\Poedit\GettextTools\bin;%PATH%"
@@ -22,7 +22,11 @@
 @rem -------------------
 @if exist "%ProgramFiles(x86)%\Notepad++" set "PATH=%ProgramFiles(x86)%\Notepad++;%PATH%"
 @if exist "%ProgramFiles%\Notepad++" set "PATH=%ProgramFiles%\Notepad++;%PATH%"
+@rem Git
+@rem -------------------
+@if exist "%ProgramFiles(x86)%\Git" set "PATH=%ProgramFiles(x86)%\Git\bin;%PATH%"
+@if exist "%ProgramFiles%\Git\bin" set "PATH=%ProgramFiles%\Git\bin;%PATH%"
 @rem Bakefile
 @rem -------------------
-@if exist "e:\storage\transmitterdan\bakefile-1.2.5.1_beta-win" set "PATH=e:\storage\transmitterdan\bakefile-1.2.5.1_beta-win;%PATH%"
+@if exist "C:\storage\transmitterdan\bakefile-1.2.5.1_beta-win" set "PATH=C:\storage\transmitterdan\bakefile-1.2.5.1_beta-win;%PATH%"
 @exit /B 0
