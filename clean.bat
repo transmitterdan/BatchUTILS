a1478812978786f739e9a091b0883d998d4c4aa9
transmitterdan <dldickey@ieee.org>
2017-03-26 14:43:46 -0500

    Don't delete target folder.

--- clean.bat
+++ clean.bat
@@ -3,21 +3,20 @@ rem @echo off
 if not "%1"=="release" if not "%1"=="debug" goto usage
 :ok
 cmake --build . --config %1 --target clean
-rmdir /s /q ..\build\%1
 :finish
 @endlocal
 exit /b 0
 :usage
 @echo Usage: %0  release | debug
 @echo Example 1: %0 debug
-@echo Above command will clean out the debug tree and prepare
-@echo for a build from scratch.
-@echo Note that it also removes OpenCPN .ini files so
-@echo it will be as if running OpenCPN for the first time
-@echo after you clean everything.
+@echo Above command will clean out the release or 
+@echo debug tree and prepare for a build from scratch.
+@echo Note that it also removes OpenCPN.ini so
+@echo it will be as if running the first time if
+@echo you run portable (i.e. OpenCPN -p).
 @echo It does not clean out any charts but it does remove
 @echo the information about the default chart location so
-@echo you will have to set that up next time you run the
-@echo cleaned version.
+@echo you will have to set that up next time you run 
+@echo in portable mode.
 endlocal
 exit /b /1
