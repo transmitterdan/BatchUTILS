795b621efa700508387c9d66df19077dc3c25756
transmitterdan <noone@noisp.org>
2019-10-20 13:50:07 -0500

    Add some notes about installing development environment on WSL

--- config.bat
+++ config.bat
@@ -57,12 +57,12 @@ set "var=%vcts%"
 set "search=_xp"
 CALL set "test=%%var:%search%=%%"
 
-if "%test%"=="%var%" (set "XP_FLAG="/SUBSYSTEM:WINDOWS"" ) else (set "XP_FLAG="/SUBSYSTEM:WINDOWS,5.01"")
+if "%test%"=="%var%" (set "XP_FLAG="/SUBSYSTEM:WINDOWS" " ) else (set "XP_FLAG="/SUBSYSTEM:WINDOWS,5.01" ")
 @echo XP_FLAG=%XP_FLAG%
 
 echo configuring generator %vcgen% and toolset v%vcts%
-@echo cmake -Wno-dev %A_FLAG% -G"%vcgen%" -T "v%vcts%" -D CMAKE_CXX_FLAGS="/D_USING_V110_SDK71_ /MP /EHsc" -D CMAKE_C_FLAGS="/MP /D_USING_V110_SDK71_" -D CMAKE_EXE_LINKER_FLAGS=%XP_FLAG% -D CMAKE_MODULE_LINKER_FLAGS=%XP_FLAG% -D CMAKE_SHARED_MODULE_LINKER_FLAGS=%XP_FLAG% ..
-cmake -Wno-dev "%A_FLAG%" -G"%vcgen%" -T "v%vcts%" -D CMAKE_CXX_FLAGS="/D_USING_V110_SDK71_ /MP /EHsc" -D CMAKE_C_FLAGS="/MP /D_USING_V110_SDK71_" -D CMAKE_EXE_LINKER_FLAGS=%XP_FLAG% -D CMAKE_MODULE_LINKER_FLAGS=%XP_FLAG% -D CMAKE_SHARED_MODULE_LINKER_FLAGS=%XP_FLAG% ..
+@echo cmake -Wno-dev %A_FLAG% -G"%vcgen%" -T "v%vcts%" -D CMAKE_CXX_FLAGS="/D_USING_V110_SDK71_ /MP /EHsc" -D CMAKE_C_FLAGS="/MP /D_USING_V110_SDK71_" -D CMAKE_EXE_LINKER_FLAGS="%XP_FLAG% " -D CMAKE_MODULE_LINKER_FLAGS="%XP_FLAG% " -D CMAKE_SHARED_MODULE_LINKER_FLAGS="%XP_FLAG% " ..
+cmake -Wno-dev "%A_FLAG%" -G"%vcgen%" -T "v%vcts%" -D CMAKE_CXX_FLAGS="/D_USING_V110_SDK71_ /MP /EHsc" -D CMAKE_C_FLAGS="/MP /D_USING_V110_SDK71_" -D CMAKE_EXE_LINKER_FLAGS="%XP_FLAG% " -D CMAKE_MODULE_LINKER_FLAGS="%XP_FLAG% " -D CMAKE_SHARED_MODULE_LINKER_FLAGS="%XP_FLAG% " ..
 set vcts=
 set vcgen=
 @endlocal
